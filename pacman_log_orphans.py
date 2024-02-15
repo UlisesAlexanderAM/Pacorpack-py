2b67b94e8b9b528cdf9115112f06b97e3d412d34
Ulises Alexander AM <30351520+UlisesAlexanderAM@users.noreply.github.com>
2024-02-15 18:17:08 +0000

    fix: add exception handling

--- pacman_log_orphans.py
+++ pacman_log_orphans.py
@@ -2,20 +2,29 @@
 
 import subprocess
 
-orphan_packages_command = subprocess.run(args=["pacman","-Qdtq"],capture_output=True,check=True)
-orphan_packages = orphan_packages_command.stdout.decode("utf-8")
-msg = f"""
-The following packages are installed but not required (anymore):
-{orphan_packages}
-You can mark them as explicitly installed with \'pacman  -D --asexplicit <pkg>\'
-or remove them all using \'pacman -Qdtq | pacman -Rns\'"""
-no_pkgs_msg="No orphan packages found"
 
 def main():
-    if orphan_packages_command.returncode == 0:
+    try:
+        orphan_packages_command = subprocess.run(
+            args=["pacman", "-Qdtq"], capture_output=True, check=True, encoding="utf-8"
+        )
+        orphan_packages = orphan_packages_command.stdout
+        msg = f"""
+        The following packages are installed but not required (anymore):
+        {orphan_packages}
+        You can mark them as explicitly installed with \'pacman  -D --asexplicit <pkg>\'
+        or remove them all using \'pacman -Qdtq | pacman -Rns\'"""
         print(msg)
-    else:
-        print(no_pkgs_msg)
+    except subprocess.CalledProcessError as exc:
+        if exc.returncode == 1:
+            print("No orphan packages found")
+        else:
+            print(
+                "Procces failed because did not return a succesful return code."
+                f"Returned {exc.returncode}"
+                f"{exc}"
+            )
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
